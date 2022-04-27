ab027e05387162fdce3ef8b21a6a9cb9293698a2
Emelyanov Roman <wohagy@gmail.com>
2022-04-27 21:38:42 +0300

    initial commit

---
+++ Armaggedon 2022/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  Armaggedon 2022
+//
+//  Created by Macbook on 27.04.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
---
+++ Armaggedon 2022/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Armaggedon 2022
+//
+//  Created by Macbook on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
